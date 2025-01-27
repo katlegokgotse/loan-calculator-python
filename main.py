7b05aa2dd9a4b07d16ec7cc28079bea046727eda
Katlego Kgotse <katlegokgotse88@gmail.com>
2025-01-27 12:57:51 +0200

    Main: User control options

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 # write your code here
+# Enter the loan principal:
 print("Enter the loan principal:")
 principalAmount = int(input())
-print("Enter the monthly payment:")
-monthlyPayments = int(input())
-repayments = principalAmount // monthlyPayments
-print(f"It will take {repayments} months to repay the loan")
+
+print("What do you want to calculate?")
+typePayment = input()
+repayments = 0
+while True:
+    if (typePayment == "m"):
+        print("Enter the monthly payment:")
+        monthlyPayments = int(input())
+        repayments= principalAmount // monthlyPayments
+        print(f"It will take {repayments} months to repay the loan")
+    elif (typePayment == "p"):
+        print("Enter the number of months:")
+        months = int(input())
+        payments 
+        repayments = principalAmount - (months - 1) * 
+        print(f"Your monthly payment = {repayments}")
+    print("What do you want to calculate?")
+    typePayment = input()
+
+
+#Formula : principal−(periods−1)⋅payment
