22461bd19d8ff2bc90a309fc9f36c0b3c76376f4
Katlego Kgotse <katlegokgotse88@gmail.com>
2025-01-30 13:08:49 +0200

    Main: Knowing your math functions comes in handy for this kind of project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # write your code here
 # Enter the loan principal:
+import math
 print("Enter the loan principal:")
 principalAmount = int(input())
 
@@ -7,18 +8,20 @@ print("What do you want to calculate?")
 print("type \"m\" for number of monthly payments,")
 print("type \"p\" for the monthly payment:") 
 typePayment = input()
-repayments = 0
 if (typePayment == "m"):
         print("Enter the monthly payment:")
         monthlyPayments = int(input())
-        repayments= principalAmount // monthlyPayments
-        print(f"It will take {repayments} months to repay the loan")
+        repayments= principalAmount / monthlyPayments
+        if principalAmount % monthlyPayments != 0:
+            repayments +=1
+        print(f"It will take {math.floor(repayments)} months to repay the loan")
 elif (typePayment == "p"):
         print("Enter the number of months:")
         months = int(input())
-        basePay = principalAmount // (months)
-        repayments = principalAmount - (months - 1) * basePay
+        basePay = principalAmount / months
+        lastPayment = 0
+        lastPayment = principalAmount - (math.ceil(basePay) * (months - 1))
         if (principalAmount % months == 0):
             print(f"Your monthly payment = {basePay}")
         else: 
-            print(f"Your monthly payment = {basePay + 1} and the last payment = {repayments}")
+            print(f"Your monthly payment = {math.ceil(basePay)} and the last payment = {lastPayment}")
